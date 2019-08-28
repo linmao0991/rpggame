057c44364717aa7d30de0533e6507cbdca3d15ae
Mao Lin <lin.maoxian0991@gmail.com>
2019-08-27 23:00:01 -0400

    Adding index.html css files and game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function(){
             mp: 100, 
             atk: 10, 
             def: 10,
-            items: ["rocks","dirt","iron","coal",],
+            items: ["rocks","dirt","iron","coal"],
         },
         {
             name: "waterGolem",
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function(){
             mp: 200, 
             atk: 5, 
             def: 5,
-            item: [""],
+            items: ["water","ice","mist in a bottle"],
         },
         {
             name: "fireGolem",
@@ -22,9 +22,34 @@ document.addEventListener('DOMContentLoaded', function(){
             mp: 100,
             atk: 15,
             def: 15,
-            items: [""],
-        }
+            items: ["fire", "coal", "lava"],
+        },
+        {
+            name: "giantCrab",
+            hp: 100,
+            mp: 100,
+            atk: 6,
+            def: 6,
+            items: ["crab meat", "giant crab claw", "crab shell"],
+        },
     ]
-
-    console.log(npcInfo[0].items[3]);
+   
+    var playerInfo = {
+        name: "",
+        hp: 100,
+        mp: 100,
+        atk: 10,
+        def: 10,
+        inventory: [""],
+    }
+    console.log(npcInfo[1].name);
+    monster = Math.floor(Math.random() * npcInfo.length);
+    console.log(monster);
+    itemIndex = Math.floor(Math.random() * npcInfo[monster].items.length);
+    console.log(itemIndex);
+    // itemDrop = 
+    // console.log(itemDrop);
+    // console.log(npcInfo[Math.floor(Math.random() * npcInfo.length)].items[Math.floor(Math.random() * this.items.length)]);
+    // playerInfo.inventory.push()
+    // console.log(npcInfo[Math.random(Math.floor * npcInfo.length)].items[3]);
 });
