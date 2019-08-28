5ee7e196e771f53f04d99ddeae46298476dca127
linmao0991 <lin.maoxian0991@gmail.com>
2019-08-28 00:49:11 -0400

    Added html code to frame out the layout of the page. Added a tabbed window to the page.
    Added more data into the js file. Changed the data arrays to all objects within objects.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,112 @@
 document.addEventListener('DOMContentLoaded', function(){
-    var npcInfo = [
-        {
+    var playerHP = 0;
+    var playerMP = 0;
+    var npcHp = 0;
+    var npcMP = 0;
+
+
+    var npcInfo = {
+        rockGolem: {
             name: "rockGolem",
-            hp: 200, 
-            mp: 100, 
+            maxHp: 200, 
+            maxMp: 100, 
             atk: 10, 
             def: 10,
-            items: ["rocks","dirt","iron","coal"],
+            inventory: [],
         },
-        {
-            name: "waterGolem",
-            hp: 300, 
+        ironGolem: {
+            name: "ironGolem",
+            maxMp: 300, 
             mp: 200, 
             atk: 5, 
             def: 5,
-            items: ["water","ice","mist in a bottle"],
+            inventory: [],
         },
-        {
+        fireGolem: {
             name: "fireGolem",
-            hp: 150,
+            maxMp: 150,
             mp: 100,
             atk: 15,
             def: 15,
-            items: ["fire", "coal", "lava"],
+            inventory: [],
         },
-        {
+        giantCrab: {
             name: "giantCrab",
-            hp: 100,
+            maxMp: 100,
             mp: 100,
             atk: 6,
             def: 6,
-            items: ["crab meat", "giant crab claw", "crab shell"],
+            inventory: [],
         },
-    ]
-   
+    }
+    // item Data
+    // iValue is the gold value of the item
+    // iType is the type of item
+    //      0 = misc item
+    //      1 = consumable
+    //      2 = weapon
+    //      3 = helmet
+    //      4 = armor
+    var itemData = {
+        iron: {
+            iD: 00,
+            iName: "Iron",
+            iValue: 10,
+            iType: 0,
+        },
+        meat:{
+            iD: 01,
+            iName: "Meat",
+            iValue: 1,
+            iType: 1,
+        },
+        ironSword: {
+            iD: 02,
+            iName: "Iron Sword",
+            iValue: 50,
+            iType: 2,
+        },
+        ironHelm: {
+            iD: 03,
+            iName: "Iron Helm",
+            iValue: 50,
+            iType: 3,
+        },
+        ironArmor: {
+            iD: 04,
+            iName: "Iron Armor",
+            iValue: 50,
+            iType: 4,
+        }
+    }
+
+    var spellData = {
+        fire: {
+            dmg: 10,
+            mpCost: 10,
+        },
+        water: {
+            dmg: 10,
+            mpCost: 10,
+        },
+        earth: {
+            dmg: 10,
+            mpCost: 10,
+        },
+        wind: {
+            dmg: 10,
+            mpCost: 10,
+        },
+    }
+
     var playerInfo = {
         name: "",
-        hp: 100,
-        mp: 100,
+        maxHp: 100,
+        maxMp: 100,
         atk: 10,
         def: 10,
-        inventory: [""],
+        gold: 0,
+        inventory: [],
     }
-    console.log(npcInfo[1].name);
-    monster = Math.floor(Math.random() * npcInfo.length);
-    console.log(monster);
-    itemIndex = Math.floor(Math.random() * npcInfo[monster].items.length);
-    console.log(itemIndex);
-    // itemDrop = 
-    // console.log(itemDrop);
-    // console.log(npcInfo[Math.floor(Math.random() * npcInfo.length)].items[Math.floor(Math.random() * this.items.length)]);
-    // playerInfo.inventory.push()
-    // console.log(npcInfo[Math.random(Math.floor * npcInfo.length)].items[3]);
+
 });
